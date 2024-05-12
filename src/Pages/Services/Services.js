247131b429d228ac794ebf4bf9dfa16d12d6779b
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-05-13 02:03:49 +0530

    Add services page

---
+++ src/Pages/Services/Services.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import "./Services.css"
+import corporate from "../../Assets/CORPORATE 2.jpg"
+import hospital from "../../Assets/hospital.jpg"
+import govt from "../../Assets/goverment.jpg"
+import airam from "../../Assets/airambulance.jpg"
+import individual from "../../Assets/individuls_infoG.svg"
+const Services = () => {
+  return (
+    <>
+        <div className='heading_services'>
+          <h2>Book Emergency Ambulance Service with Best Facilities</h2>
+          <p>Book Ambulance for different services in Hospitals,Corporate and Goverments.</p>
+        </div>
+   <div className='cards-container'>
+   <div className='cards'>
+      <div className='card1'>
+            <img src={corporate} alt="Card1"/>
+            <div class="card-body">
+                <h5>Corporate</h5>
+                <p>Sevaride is working with different corporate for providing ambulance. </p>
+                <button>Know more</button>
+            </div>
+        </div>
+        <div className='card1'>
+            <img src={hospital} alt="Card1"/>
+            <div class="card-body">
+                <h5>Hospitals</h5>
+                <p>Sevaride is working with different corporate for providing ambulance. </p>
+                <button>Know more</button>
+            </div>
+        </div>
+        <div className='card1'>
+            <img src={govt} alt="Card1"/>
+            <div class="card-body">
+                <h5>Goverments</h5>
+                <p>Sevaride is working with different corporate for providing ambulance. </p>
+                <button>Know more</button>
+            </div>
+        </div>
+        <div className='card1'>
+            <img src={airam} alt="Card1"/>
+            <div class="card-body">
+                <h5>Corporate</h5>
+                <p>Sevaride is working with different corporate for providing ambulance. </p>
+                <button>Know more</button>
+            </div>
+        </div>
+        <div className='card2'>
+            <img src={individual} alt="Card2"/>
+            <div class="card2-body">
+                <h5>Individual</h5>
+                <p>Sevaride also has a 24*7*365 days functional emergency helpline number present across pan India, where people can reach out to us for ambulances Support. </p>
+                <button>Know more</button>
+                <button>Helpline</button>
+            </div>
+        </div>
+    </div>
+   </div>
+    </>
+  )
+}
+
+export default Services
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,7 +3,7 @@ import About from "./AboutUs/About";
 import Login from "./Login/Login";
 import Error from "./Error/Error";
 import BookNow from "./BookNow/BookNow";
+import Services from "./Services/Services"
 
 
-
-export {Contact,About,Login,Error,BookNow};
+export {Contact,About,Login,Error,BookNow,Services};
