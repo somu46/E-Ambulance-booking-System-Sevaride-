d4986695203aaf22cc1bdd0109d30c3b1947eba7
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-05-14 08:59:07 +0530

    update services page

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -8,13 +8,14 @@ import individual from "../../Assets/individuls_infoG.svg"
 const Services = () => {
   return (
     <>
-        <div className='heading_services'>
+    <div className='flex flex-wrap justify-center items-center m-1 p-1'>
+        <div className='heading_services '>
           <h2>Book Emergency Ambulance Service with Best Facilities</h2>
           <p>Book Ambulance for different services in Hospitals,Corporate and Goverments.</p>
         </div>
-   <div className='cards-container'>
+   <div className='cards-container p-3'>
    <div className='cards'>
-      <div className='card1'>
+      <div className='card1 '>
             <img src={corporate} alt="Card1"/>
             <div class="card-body">
                 <h5>Corporate</h5>
@@ -46,9 +47,9 @@ const Services = () => {
                 <button>Know more</button>
             </div>
         </div>
-        <div className='card2'>
+        <div className='card2 bg-red-100'>
             <img src={individual} alt="Card2"/>
-            <div class="card2-body">
+            <div className=" card2-body"> 
                 <h5>Individual</h5>
                 <p>Sevaride also has a 24*7*365 days functional emergency helpline number present across pan India, where people can reach out to us for ambulances Support. </p>
                 <button>Know more</button>
@@ -56,6 +57,7 @@ const Services = () => {
             </div>
         </div>
     </div>
+   </div>
    </div>
     </>
   )
