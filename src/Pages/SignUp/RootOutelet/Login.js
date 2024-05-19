76e4d66e2a4767db30341f4200b6dba154578afc
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-05-19 16:02:53 +0530

    nothing to commit

--- src/Pages/SignUp/RootOutelet/Login.js
+++ src/Pages/SignUp/RootOutelet/Login.js
@@ -18,19 +18,23 @@ const Login = () => {
     <>
       <div className="container">
         <div className="user-container">
-          <div onClick={handlePatientClick}>
+          <div >
+            {/* <from action='/login/user' method="GET" > */}
             <button
+            onClick={handlePatientClick}
               type="button"
               className=" flex justify-center align-middle w-[220px] h-[60px] p-3 rounded-md text-xl ml-10 mr-auto bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ..."
             >
              <p className="font-semibold text-xl p-1 text-black">Patient</p>
             </button>
+            {/* </from> */}
           </div>
           <div
-            onClick={handleDriverClick}
+          
           >
           
             <button
+              onClick={handleDriverClick}
               type="button"
               className=" flex justify-center align-middle w-[220px] h-[60px] p-3 rounded-md text-xl ml-10 mr-auto bg-gradient-to-r from-pink-500 to-yellow-500 hover:from-green-400 hover:to-blue-500 "
             >
