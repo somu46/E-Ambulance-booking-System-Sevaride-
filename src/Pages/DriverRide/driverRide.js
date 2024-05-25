53bb6ce90ac8d110f596a7113efefed43216a1d3
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-05-26 04:24:50 +0530

    new driverRide page added

---
+++ src/Pages/DriverRide/driverRide.js
@@ -0,0 +1,14 @@
+/* After logged in dfriver will get into this page where he will receive the ride details and if accepted the ride the map show up 
+the direction to the pickup location*/ 
+
+import React from 'react'
+import './driverRide.css'
+const driverRide = () => {
+  return (
+    <div>
+        <h1>This page is now under Development</h1>
+    </div>
+  )
+}
+
+export default driverRide
